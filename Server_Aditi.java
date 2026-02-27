464db6404b6dc63a7361c4263da80aaaed658802
aditibaghel9 <bagheladiti99@gmail.com>
2026-02-27 03:32:56 +0000

    adding server

---
+++ Server_Aditi.java
@@ -0,0 +1,61 @@
+import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketImpl;
+
+public class Server_Aditi{
+    public static void main(String[] args, SocketImpl port) {
+        
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket serverSocket = null;
+
+        serverSocket = new ServerSocket(port 5050);
+
+        while (true) { 
+            
+            try {
+                socket = serverSocket.accept();
+
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+                bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+                while (true) { 
+                    
+                    String msgFromClient = bufferedReader.readLine();
+
+                    System.out.println("Client: " + msgFromClient);
+
+                    bufferedWriter.write("MSG Received");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    if (msgFromClient.equalsIgnoreCase("BYE")){
+                        break;
+                    }
+
+                    socket.close();
+                    inputStreamReader.close();
+                    outputStreamWriter.close();
+                    bufferedReader.close();
+                    bufferedWriter.close();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    }
