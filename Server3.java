1141c3b9533174a43d2ec50cfb58976d03b4aeae
aditibaghel9 <asbaghel@quinnipiac.edu>
2026-02-28 16:37:34 -0500

    added more server's

---
+++ Server3.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+
+public class Server3{
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5050)) {
+            System.out.println("Server started on port 5050...");
+            //
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+
+                    while (true) {
+                        String msgFromClient = bufferedReader.readLine();
+
+                        if (msgFromClient == null) {
+                            System.out.println("Client disconnected.");
+                            break;
+                        }
+
+                        System.out.println("Client: " + msgFromClient);
+
+                        bufferedWriter.write("MSG Received");
+                        bufferedWriter.newLine();
+                        bufferedWriter.flush();
+
+                        if (msgFromClient.equalsIgnoreCase("BYE")) {
+                            System.out.println("Closing connection (BYE received).");
+                            break;
+                        }
+                    }
+                } finally {
+                    socket.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
