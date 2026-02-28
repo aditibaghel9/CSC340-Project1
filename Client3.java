941551b49b5eae0d9dc4a0ed90e3cea9ce4875a6
aditibaghel9 <asbaghel@quinnipiac.edu>
2026-02-28 15:56:13 -0500

    added more clients

--- Client3.java
+++ Client3.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class Client_Aditi1 {
+public class Client3 {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.65.2", 5050);
+        try (Socket socket = new Socket("needs IP address for vm 3", 5050);
             //192.168.65.2
              BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
